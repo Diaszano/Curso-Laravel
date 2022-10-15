0ac56eb239051c26ac92d4235605d6ffbb2958f5
Diaszano <lucasdiiassantos@gmail.com>
2022-10-15 04:00:07 +0000

    Criado layout com o blade e adicionado Bootstrap

---
+++ curso-laravel/resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        {{-- Fonte do Google --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        {{-- CSS Bootstrap --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+        {{-- CSS da Aplicacão --}}
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+        @yield('content')
+        <footer>
+            <p>Diaszano &copy; 2022</p>
+        </footer>
+    </body>
+</html>
--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
+ 
     $nome = "Lucas Dias";
     $arr = [1,2,3,4,5];
     $nomes = ['Lucas', 'Raila', 'Luana'];
