712a1dd652e0a4a4b242555fc7580c0f1476786e
Diaszano <lucasdiiassantos@gmail.com>
2022-10-21 01:15:15 +0000

    add login in project

--- curso-laravel/app/Http/Controllers/EventController.php
+++ curso-laravel/app/Http/Controllers/EventController.php
@@ -11,9 +11,19 @@ class EventController extends Controller
 {
     public function index(){
 
-        $events = Event::all();
+        $search = request('search');
 
-        return view('welcome',['events' => $events]);
+        if($search){
+            $events = Event::where([
+                // [Event::raw('lower(title)'),'like', '%'.strtolower($search).'%']
+                ['title','like', '%'.$search.'%']
+            ])->get();
+        }else{
+            $events = Event::all();
+        }
+
+
+        return view('welcome',['events' => $events, 'search' => $search]);
     }
 
     public function create(){
--- curso-laravel/resources/views/welcome.blade.php
+++ curso-laravel/resources/views/welcome.blade.php
@@ -5,14 +5,18 @@
 
 <div id="search-container" class="col-md-12">
     <h1>Busque um evento</h1>
-    <form action="">
+    <form action="/" method='GET'>
         <input type="text" id='search' name="search" class='form-control'
             placeholder="Procurar...">
     </form>
 </div>
 <div id='events-container' class='col-md-12'>
-    <h2>Próximos eventos</h2>
-    <p>Veja os eventos dos próximos dias</p>
+    @if($search)
+        <h2>Buscando por: {{$search}}</h2>
+    @else
+        <h2>Próximos eventos</h2>
+        <p>Veja os eventos dos próximos dias</p>
+    @endif
     <div id='cards-container' class='row'>
         @foreach($events as $event)
             <div class='card col-md-3'>
@@ -37,7 +41,13 @@
             </div>
         @endforeach
         @if(count($events) == 0)
-            <p>Não contém eventos disponíveis!</p>
+            @if($search)
+                <p>Não foi possível encontrar nenhum evento com '{{$search}}'!
+                    <a href='/'>Ver todos</a>
+                </p>
+            @else
+                <p>Não contém eventos disponíveis!</p>
+            @endif
         @endif
     </div>
 </div>
